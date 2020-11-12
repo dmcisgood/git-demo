42b759f06e4ef94501ef4dafd063ca0efdeda6ae
dmcisgood <mingchen.dong@qq.com>
2020-11-12 13:06:43 +0800

    第二次git提交

--- git_demo1/src/com/test1/TestMain1.java
+++ git_demo1/src/com/test1/TestMain1.java
@@ -3,5 +3,7 @@ package com.test1;
 public class TestMain1 {
     public static void main(String[] args) {
         System.out.println("git is good");
+
+        System.out.println("dmc is good");
     }
 }
